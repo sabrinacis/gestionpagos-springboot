9738495e0b6b75c534e92faf27454a5466e83713
Sabrina Cis <sabri.cis1802@gmail.com>
2019-09-10 01:31:29 -0300

    ftp camel component added

--- src/main/java/lu/lllc/boottest/controller/MainController.java
+++ src/main/java/lu/lllc/boottest/controller/MainController.java
@@ -1,5 +1,6 @@
 package lu.lllc.boottest.controller;
 
+import lu.lllc.boottest.ftp.CamelExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,6 +9,15 @@ public class MainController {
 	
 	@RequestMapping("/")
 	public String hello(){
+
+		CamelExecutor executor = new CamelExecutor();
+
+		try {
+			executor.init();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return "hello";
 	}
 }
